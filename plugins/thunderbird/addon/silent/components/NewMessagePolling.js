177e6b7cfcbece65aea6104559ffd70d7636ee62
Dmitry Valento <decobramegra@gmail.com>
2014-05-24 16:37:33 +0300

    Thunderbird addon: read preferences, call DLL via ctypes.

--- plugins/thunderbird/addon/silent/components/NewMessagePolling.js
+++ plugins/thunderbird/addon/silent/components/NewMessagePolling.js
@@ -1,8 +1,17 @@
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("resource://gre/modules/ctypes.jsm")
 var consoleServicwe = Components.classes['@mozilla.org/consoleservice;1'].getService(Components.interfaces.nsIConsoleService);
+var prefBranch = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+
+function log(msg) {
+	if (!prefBranch.getBoolPref("extensions.notikeys.log")) {
+		return;
+	}
+	consoleServicwe.logStringMessage(msg);
+};
 
 function InformNotikeys() {
-	consoleServicwe.logStringMessage("onLoad Blocker");
+	log("onLoad Blocker");
 };
 
 InformNotikeys.prototype = {
@@ -13,22 +22,62 @@ InformNotikeys.prototype = {
 	QueryInterface: XPCOMUtils.generateQI(),
 };
 
+const SRV_RSS = "rss";
+
 var event = {
 	notify: function(timer) {
-		consoleServicwe.logStringMessage("onLoad notify");
+		log("onLoad notify");
 		var acctMgr = Components.classes["@mozilla.org/messenger/account-manager;1"].getService(Components.interfaces.nsIMsgAccountManager);
 		var accounts = acctMgr.accounts;
-		consoleServicwe.logStringMessage("accounts");
 		if (accounts.queryElementAt) {
 			// Gecko 17+
-			consoleServicwe.logStringMessage("Gecko 17+");
+			log("Gecko 17+");
 			for (var i = 0; i < accounts.length; i++) {
 				var account = accounts.queryElementAt(i, Components.interfaces.nsIMsgAccount);
 				var rootFolder = account.incomingServer.rootFolder; // nsIMsgFolder
-				consoleServicwe.logStringMessage("Root folder: " + rootFolder.prettiestName + ". Unread: " + rootFolder.getNumUnread(true));
-				consoleServicwe.logStringMessage("Root folder: " + rootFolder.prettiestName + ". Flags: 0x" + rootFolder.flags.toString(16) + ". Server type: " + rootFolder.server.type);
+				log("Root folder: " + rootFolder.prettiestName + ". Unread: " + rootFolder.getNumUnread(true));
+				log("Root folder: " + rootFolder.prettiestName + ". Flags: 0x" + rootFolder.flags.toString(16) + ". Server type: " + rootFolder.server.type);
+				// RSS
+				if (rootFolder.server.type == SRV_RSS) {
+					if (prefBranch.getBoolPref("extensions.notikeys.showrss")) {
+						log("RSS feed checked");
+					} else {
+						log("RSS feed skipped");
+					}
+				} else {
+					log("no RSS feed");
+				}
+/*				if (rootFolder.hasSubFolders) {
+					var subFolders = rootFolder.subFolders; // nsIMsgFolder
+					while (subFolders.hasMoreElements()) {
+						var folder = subFolders.getNext().QueryInterface(Components.interfaces.nsIMsgFolder);
+						log("Subfolder: " + folder.prettiestName + ".Unread: " + rootFolder.getNumUnread(true));
+					}
+				}
+*/				// Do something with account
+			}
+		} else {
+			// Gecko < 17
+			for (var i = 0; i < accounts.Count(); i++) {
+				var account = accounts.QueryElementAt(i, Components.interfaces.nsIMsgAccount);
+				// Do something with account
 			}
 		}
+		var lib = ctypes.open("C:\\WINDOWS\\system32\\user32.dll");
+		
+		/* Declare the signature of the function we are going to call */
+		var msgBox = lib.declare("MessageBoxW",
+		                         ctypes.winapi_abi,
+		                         ctypes.int32_t,
+		                         ctypes.int32_t,
+		                         ctypes.jschar.ptr,
+		                         ctypes.jschar.ptr,
+		                         ctypes.int32_t);
+		var MB_OK = 0;
+		
+		var ret = msgBox(0, "Hello world", "title_me", MB_OK);
+		
+		lib.close();
 	}
 }
 
@@ -38,7 +87,7 @@ else
 	const NSGetModule = XPCOMUtils.generateNSGetModule([InformNotikeys]); //for Gecko 1.9.*
 
 var consoleService = Components.classes['@mozilla.org/consoleservice;1'].getService(Components.interfaces.nsIConsoleService);
-consoleService.logStringMessage("onLoad XPCOMUtils");
+log("onLoad XPCOMUtils");
 
 var timer = Components.classes["@mozilla.org/timer;1"].createInstance(Components.interfaces.nsITimer);
-timer.initWithCallback(event, 3000, Components.interfaces.nsITimer.TYPE_REPEATING_PRECISE_CAN_SKIP);
+timer.initWithCallback(event, 5000, Components.interfaces.nsITimer.TYPE_REPEATING_PRECISE_CAN_SKIP);

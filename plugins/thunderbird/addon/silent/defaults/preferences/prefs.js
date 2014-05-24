862cbc6b025b11f9ef48e671b77b7d5174a0ba20
Dmitry Valento <decobramegra@gmail.com>
2014-05-24 16:14:16 +0300

    Thunderbird addon: Add icons, preferences, translations. Rename plugin and script.

---
+++ plugins/thunderbird/addon/silent/defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.notikeys.shownews", false);
+pref("extensions.notikeys.showrss", false);
+pref("extensions.notikeys.log", false);
+pref("extensions.notikeys.restore", true);
+pref("extensions.notikeys.enable", true);
+pref("extensions.notikeys.timeoutAfterTrayRestore", 2000);

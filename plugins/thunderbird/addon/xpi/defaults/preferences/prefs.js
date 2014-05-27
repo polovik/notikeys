7d5e3fc1fdab7ba933fe3a692fe1327bcc89d7ec
polovik <decobramegra@gmail.com>
2014-05-27 11:28:33 +0300

    Thunderbird plugin: move folder up.

--- plugins/thunderbird/addon/xpi/defaults/preferences/prefs.js
+++
@@ -1,6 +0,0 @@
-pref("extensions.notikeys.shownews", false);
-pref("extensions.notikeys.showrss", false);
-pref("extensions.notikeys.log", false);
-pref("extensions.notikeys.restore", true);
-pref("extensions.notikeys.enable", true);
-pref("extensions.notikeys.timeoutAfterTrayRestore", 2000);
